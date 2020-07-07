0ed3d615d65c99e3aefec539823305374eb7e36c
Heriyanto <heriyanto.liu@gmail.com>
2020-07-07 10:00:46 +0700

    chapter 10.2

--- common/messaging/messagingclient.go
+++ common/messaging/messagingclient.go
@@ -1,7 +1,8 @@
 package messaging
 
-import(
+import (
 	"fmt"
+
 	"github.com/sirupsen/logrus"
 	"github.com/streadway/amqp"
 )
@@ -36,45 +37,45 @@ func (m *MessagingClient) Publish(body []byte, exchangeName string, exchangeType
 	if m.conn == nil {
 		panic("Tried to send message before connection was initialized. Don't do that.")
 	}
-	ch, err := m.conn.Channel()      // Get a channel from the connection
+	ch, err := m.conn.Channel() // Get a channel from the connection
 	defer ch.Close()
 	err = ch.ExchangeDeclare(
 		exchangeName, // name of the exchange
 		exchangeType, // type
-		true, // durable
-		false, // delete when complete
-		false, // internal
-		false, // noWait
-		nil, // arguments
+		true,         // durable
+		false,        // delete when complete
+		false,        // internal
+		false,        // noWait
+		nil,          // arguments
 	)
 	failOnError(err, "Failed to register an Exchange")
 
-	queue, err := ch.QueueDeclare(// Declare a queue that will be created if not exists with some args
-		"", // our queue name
+	queue, err := ch.QueueDeclare( // Declare a queue that will be created if not exists with some args
+		"",    // our queue name
 		false, // durable
 		false, // delete when unused
 		false, // exclusive
 		false, // no-wait
-		nil, // arguments
+		nil,   // arguments
 	)
 
 	err = ch.QueueBind(
-		queue.Name, // name of the queue
+		queue.Name,   // name of the queue
 		exchangeName, // bindingKey
 		exchangeName, // sourceExchange
-		false, // noWait
-		nil, // arguments
-	);
+		false,        // noWait
+		nil,          // arguments
+	)
 
-	err = ch.Publish(// Publishes a message onto the queue.
+	err = ch.Publish( // Publishes a message onto the queue.
 		exchangeName, // exchange
 		exchangeName, // routing key      q.Name
-		false, // mandatory
-		false, // immediate
+		false,        // mandatory
+		false,        // immediate
 		amqp.Publishing{
-			Body:        body, // Our JSON body as []byte
+			Body: body, // Our JSON body as []byte
 		})
-	fmt.f("A message was sent: %v", body)
+	logrus.Infof("A message was sent: %v", body)
 	return err
 }
 
@@ -82,24 +83,24 @@ func (m *MessagingClient) PublishOnQueue(body []byte, queueName string) error {
 	if m.conn == nil {
 		panic("Tried to send message before connection was initialized. Don't do that.")
 	}
-	ch, err := m.conn.Channel()      // Get a channel from the connection
+	ch, err := m.conn.Channel() // Get a channel from the connection
 	defer ch.Close()
 
-	queue, err := ch.QueueDeclare(// Declare a queue that will be created if not exists with some args
+	queue, err := ch.QueueDeclare( // Declare a queue that will be created if not exists with some args
 		queueName, // our queue name
-		false, // durable
-		false, // delete when unused
-		false, // exclusive
-		false, // no-wait
-		nil, // arguments
+		false,     // durable
+		false,     // delete when unused
+		false,     // exclusive
+		false,     // no-wait
+		nil,       // arguments
 	)
 
 	// Publishes a message onto the queue.
 	err = ch.Publish(
-		"", // exchange
+		"",         // exchange
 		queue.Name, // routing key
-		false, // mandatory
-		false, // immediate
+		false,      // mandatory
+		false,      // immediate
 		amqp.Publishing{
 			ContentType: "application/json",
 			Body:        body, // Our JSON body as []byte
@@ -116,22 +117,22 @@ func (m *MessagingClient) Subscribe(exchangeName string, exchangeType string, co
 	err = ch.ExchangeDeclare(
 		exchangeName, // name of the exchange
 		exchangeType, // type
-		true, // durable
-		false, // delete when complete
-		false, // internal
-		false, // noWait
-		nil, // arguments
+		true,         // durable
+		false,        // delete when complete
+		false,        // internal
+		false,        // noWait
+		nil,          // arguments
 	)
 	failOnError(err, "Failed to register an Exchange")
 
 	logrus.Infof("declared Exchange, declaring Queue (%s)", "")
 	queue, err := ch.QueueDeclare(
-		"", // name of the queue
+		"",    // name of the queue
 		false, // durable
 		false, // delete when usused
 		false, // exclusive
 		false, // noWait
-		nil, // arguments
+		nil,   // arguments
 	)
 	failOnError(err, "Failed to register an Queue")
 
@@ -139,24 +140,24 @@ func (m *MessagingClient) Subscribe(exchangeName string, exchangeType string, co
 		queue.Messages, queue.Consumers, exchangeName)
 
 	err = ch.QueueBind(
-		queue.Name, // name of the queue
+		queue.Name,   // name of the queue
 		exchangeName, // bindingKey
 		exchangeName, // sourceExchange
-		false, // noWait
-		nil, // arguments
-	);
+		false,        // noWait
+		nil,          // arguments
+	)
 	if err != nil {
 		return fmt.Errorf("Queue Bind: %s", err)
 	}
 
 	msgs, err := ch.Consume(
-		queue.Name, // queue
+		queue.Name,   // queue
 		consumerName, // consumer
-		true, // auto-ack
-		false, // exclusive
-		false, // no-local
-		false, // no-wait
-		nil, // args
+		true,         // auto-ack
+		false,        // exclusive
+		false,        // no-local
+		false,        // no-wait
+		nil,          // args
 	)
 	failOnError(err, "Failed to register a consumer")
 
@@ -171,22 +172,22 @@ func (m *MessagingClient) SubscribeToQueue(queueName string, consumerName string
 	logrus.Infof("Declaring Queue (%s)", queueName)
 	queue, err := ch.QueueDeclare(
 		queueName, // name of the queue
-		false, // durable
-		false, // delete when usused
-		false, // exclusive
-		false, // noWait
-		nil, // arguments
+		false,     // durable
+		false,     // delete when usused
+		false,     // exclusive
+		false,     // noWait
+		nil,       // arguments
 	)
 	failOnError(err, "Failed to register an Queue")
 
 	msgs, err := ch.Consume(
-		queue.Name, // queue
+		queue.Name,   // queue
 		consumerName, // consumer
-		true, // auto-ack
-		false, // exclusive
-		false, // no-local
-		false, // no-wait
-		nil, // args
+		true,         // auto-ack
+		false,        // exclusive
+		false,        // no-local
+		false,        // no-wait
+		nil,          // args
 	)
 	failOnError(err, "Failed to register a consumer")
 
