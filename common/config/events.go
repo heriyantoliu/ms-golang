b3a5a13e779eeb3f75b553cf79ca0669b7f8ecae
Heriyanto <heriyanto.liu@gmail.com>
2020-07-07 10:40:00 +0700

    chapter 10.4

--- common/config/events.go
+++ common/config/events.go
@@ -2,8 +2,6 @@ package config
 
 import (
 	"encoding/json"
-	"fmt"
-	"log"
 	"strings"
 
 	"github.com/sirupsen/logrus"
@@ -11,136 +9,32 @@ import (
 	"github.com/streadway/amqp"
 )
 
-type Consumer struct {
-	conn    *amqp.Connection
-	channel *amqp.Channel
-	tag     string
-	done    chan error
+func HandleRefreshEvent(d amqp.Delivery) {
+	body := d.Body
+	consumerTag := d.ConsumerTag
+	updateToken := &UpdateToken{}
+	err := json.Unmarshal(body, updateToken)
+	if err != nil {
+		logrus.Printf("Problem parsing UpdateToken: %v", err.Error())
+	} else {
+		if strings.Contains(updateToken.DestinationService, consumerTag) {
+			logrus.Println("Reloading Viper config from Spring Cloud Config server")
+
+			// Consumertag is same as application name.
+			LoadConfigurationFromBranch(
+				viper.GetString("configServerUrl"),
+				consumerTag,
+				viper.GetString("profile"),
+				viper.GetString("configBranch"))
+		}
+	}
 }
 
+// {"type":"RefreshRemoteApplicationEvent","timestamp":1494514362123,"originService":"config-server:docker:8888","destinationService":"xxxaccoun:**","id":"53e61c71-cbae-4b6d-84bb-d0dcc0aeb4dc"}
 type UpdateToken struct {
 	Type               string `json:"type"`
 	Timestamp          int    `json:"timestamp"`
 	OriginService      string `json:"originService"`
 	DestinationService string `json:"destinationService"`
 	Id                 string `json:"id"`
-}
-
-func NewConsumer(amqpUri, exchange, exchangeType, queue, key, ctag string) error {
-	c := &Consumer{
-		conn:    nil,
-		channel: nil,
-		tag:     ctag,
-		done:    make(chan error),
-	}
-
-	var err error
-
-	logrus.Infof("dialing %s", amqpUri)
-	c.conn, err = amqp.Dial(amqpUri)
-	if err != nil {
-		return fmt.Errorf("Dial: %s", err)
-	}
-
-	logrus.Infof("got Connection, getting Channel")
-	c.channel, err = c.conn.Channel()
-	if err != nil {
-		return fmt.Errorf("Channel: %s", err)
-	}
-
-	logrus.Infof("got Channel, declaring Exchange (%s)", exchange)
-	if err = c.channel.ExchangeDeclare(
-		exchange,
-		exchangeType,
-		true,
-		false,
-		false,
-		false,
-		nil,
-	); err != nil {
-		return fmt.Errorf("Exchange Declare: %s", err)
-	}
-
-	logrus.Infof("declared Exchange, declaring Queue (%s)", queue)
-	state, err := c.channel.QueueDeclare(
-		queue,
-		false,
-		false,
-		false,
-		false,
-		nil)
-	if err != nil {
-		return fmt.Errorf("Queue Declare: %s", err)
-	}
-
-	logrus.Infof("declared Queue (%d messages, %d consumers), bidding to Exchange (key '%s')",
-		state.Messages, state.Consumers, key)
-
-	if err = c.channel.QueueBind(
-		queue,
-		key,
-		exchange,
-		false,
-		nil); err != nil {
-		return fmt.Errorf("Queue Bind: %s", err)
-	}
-
-	logrus.Infof("Queue bound to Exchange, starting Consume (consumer tag '%s')", c.tag)
-	deliveries, err := c.channel.Consume(
-		queue,
-		c.tag,
-		false,
-		false,
-		false,
-		false,
-		nil,
-	)
-	if err != nil {
-		return fmt.Errorf("Queue Consume: %s", err)
-	}
-
-	go handle(deliveries, c.done)
-
-	return nil
-
-}
-
-func handle(deliveries <-chan amqp.Delivery, done chan error) {
-	for d := range deliveries {
-		logrus.Infof("got %dB consumer: [%v] delivery: [%v] routingkey: [%v] %s",
-			len(d.Body), d.ConsumerTag, d.DeliveryTag, d.RoutingKey, d.Body)
-		handleRefreshEvent(d.Body, d.ConsumerTag)
-		d.Ack(false)
-	}
-	logrus.Infof("handle: deliveries channel closed")
-	done <- nil
-}
-
-func HandleRefreshEvent(body []byte, consumerTag string) {
-	updateToken := &UpdateToken{}
-	err := json.Unmarshal(body, updateToken)
-	if err != nil {
-		logrus.Infof("Problem parsing UpdateToken: %v", err.Error())
-	} else {
-		if strings.Contains(updateToken.DestinationService, consumerTag) {
-			logrus.Infoln("Reloading Viper config from Spring Cloud Config server")
-
-			LoadConfigurationFromBranch(
-				viper.GetString("configServerUrl"),
-				consumerTag,
-				viper.GetString("profile"),
-				viper.GetString("configBranch"),
-			)
-		}
-	}
-}
-
-func StartListener(appName string, amqpServer string, exchangeName string) {
-	err := NewConsumer(amqpServer, exchangeName, "topic", "config-event-queue", exchangeName, appName)
-	if err != nil {
-		log.Fatalf("%s", err)
-	}
-
-	logrus.Infof("running forever")
-	select {}
 }
--- common/config/loader.go
+++ common/config/loader.go
@@ -3,42 +3,42 @@ package config
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/spf13/viper"
-	"github.com/sirupsen/logrus"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
 )
 
+// Loads config from for example http://configserver:8888/accountservice/test/P8
 func LoadConfigurationFromBranch(configServerUrl string, appName string, profile string, branch string) {
 	url := fmt.Sprintf("%s/%s/%s/%s", configServerUrl, appName, profile, branch)
-	logrus.Infof("Loading config from %s\n", url)
-	body, err :=fetchConfiguration(url)
+	logrus.Printf("Loading config from %s\n", url)
+	body, err := fetchConfiguration(url)
 	if err != nil {
+		logrus.Errorf("Couldn't load configuration, cannot start. Terminating. Error: %v", err.Error())
 		panic("Couldn't load configuration, cannot start. Terminating. Error: " + err.Error())
 	}
 	parseConfiguration(body)
 }
 
 func fetchConfiguration(url string) ([]byte, error) {
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("Recovered in f", r)
+		}
+	}()
+	logrus.Printf("Getting config from %v\n", url)
 	resp, err := http.Get(url)
-	if err != nil {
+	if err != nil || resp.StatusCode != 200 {
+		logrus.Errorf("Couldn't load configuration, cannot start. Terminating. Error: %v", err.Error())
 		panic("Couldn't load configuration, cannot start. Terminating. Error: " + err.Error())
 	}
 	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic("Error reading configuration: " + err.Error())
+	}
 	return body, err
-}
-
-type springCloudConfig struct {
-	Name string `json:"name"`
-	Profiles []string `json:"profiles"`
-	Label string `json:"label"`
-	Version string `json:"version"`
-	PropertySources []propertySource `json:"propertySources"`
-}
-
-type propertySource struct {
-	Name string `json:"name"`
-	Source map[string]interface{} `json:"source"`
 }
 
 func parseConfiguration(body []byte) {
@@ -50,10 +50,22 @@ func parseConfiguration(body []byte) {
 
 	for key, value := range cloudConfig.PropertySources[0].Source {
 		viper.Set(key, value)
-		logrus.Infof("Loading config property %v => %v\n", key, value)
+		logrus.Printf("Loading config property %v => %v\n", key, value)
 	}
-
 	if viper.IsSet("server_name") {
-		logrus.Infof("Successfully loaded configuration for service %s\n", viper.GetString("server_name"))
+		logrus.Printf("Successfully loaded configuration for service %s\n", viper.GetString("server_name"))
 	}
+}
+
+type springCloudConfig struct {
+	Name            string           `json:"name"`
+	Profiles        []string         `json:"profiles"`
+	Label           string           `json:"label"`
+	Version         string           `json:"version"`
+	PropertySources []propertySource `json:"propertySources"`
+}
+
+type propertySource struct {
+	Name   string                 `json:"name"`
+	Source map[string]interface{} `json:"source"`
 }
